7239f0f705ca2afc9ca14ab2e0088a0f82c1c349
saranshsahu123 <saranshsahu532@gmail.com>
2025-10-16 10:59:19 +0530

    add new project

--- account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Applicant
-
-@admin.register(Applicant)
-class ApplicantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'mobile', 'email', 'feedback')
-    search_fields = ('name', 'email', 'mobile')
