6d6aa8c07aa488cbbc0e4c4a7eaf254470c1616c
saranshsahu123 <saranshsahu532@gmail.com>
2025-10-16 07:12:31 +0530

    add Django project

--- sitecore/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from account import views
-
-urlpatterns = [
-    path('', views.applicant_form, name='applicant_form'),
-    path('admin/', admin.site.urls),
-]
